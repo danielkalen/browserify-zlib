7c7a48b61be4367d5f4620ae5ff65907264c847e
danielkalen <d@danielkalen.com>
2017-01-02 17:17:43 -0500

    change require reference of 'readable-stream' package from '_stream_readable' to 'readable-stream'

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-var Transform = require('_stream_transform');
+var Transform = require('readable-stream');
 
 var binding = require('./binding');
 var util = require('util');
